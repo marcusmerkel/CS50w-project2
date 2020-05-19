293e522860665d006314ed17b990dc5488b42e13
Marcus Merkel <marcus@marcusmerkel.de>
2020-05-19 18:21:34 +0200

    localStorage stores displayName

--- application.py
+++ application.py
@@ -1,13 +1,19 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+users = []
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/messages")
+def messageRoom():
+    return render_template("messages.html")
